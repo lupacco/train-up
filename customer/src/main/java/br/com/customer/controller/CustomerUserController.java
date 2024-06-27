cb18d365da357f79073ea9bcb397de5a7a50d78a
lupacco <pagottolc@gmail.com>
2024-06-27 16:51:16 -0300

    feature: find customer by username flow

--- customer/src/main/java/br/com/customer/controller/CustomerUserController.java
+++ customer/src/main/java/br/com/customer/controller/CustomerUserController.java
@@ -1,8 +1,32 @@
 package br.com.customer.controller;
 
+import br.com.customer.dto.response.CustomerUserGetResponse;
+import br.com.customer.service.CustomerUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping
+@Slf4j
 public class CustomerUserController {
+
+    private final CustomerUserService customerUserService;
+
+    @GetMapping("/{customerUsername}")
+    public ResponseEntity<CustomerUserGetResponse> findById(@PathVariable(name = "customerUsername") String customerUsername){
+        log.debug("[start] CustomerUserController - findById");
+        var response = customerUserService.findByUsername(customerUsername);
+        log.debug("[finish] CustomerUserController - findById");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
 }
