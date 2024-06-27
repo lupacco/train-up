f9e25372fd8d205f15521fd77805df1203f934f9
lupacco <pagottolc@gmail.com>
2024-06-27 11:37:02 -0300

    feature: adding api v1 paths to controllers

--- customer/src/main/java/br/com/customer/controller/AuthController.java
+++ customer/src/main/java/br/com/customer/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 @Log4j2
 public class AuthController {
 
--- customer/src/main/java/br/com/customer/controller/WorkoutController.java
+++ customer/src/main/java/br/com/customer/controller/WorkoutController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/workout")
+@RequestMapping("/api/v1/workout")
 @Slf4j
 public class WorkoutController {
 
